524c04100163e5e4cb61b13d62f6d98f77f2f124
corygshava <corygshava@gmail.com>
2025-07-24 01:22:50 +0300

    finished working on the website UI kit

--- nuevo assets/js/app.js
+++ nuevo assets/js/app.js
@@ -25,6 +25,22 @@ function init() {
     })
 
     console.log(`done with visibledata, found ${items.length} ${plural("item",items.length)}`);
+
+    // setup togglers
+    items = document.querySelectorAll('[data-toggleme]');
+
+    items.forEach((el, id) => {
+        el.addEventListener('click',() => {
+            if(el.dataset.displays == undefined){
+                toggleShow(el.dataset.toggleme);
+            } else {
+                let dis = el.dataset.displays.split(",");
+                toggleShowB(el.dataset.toggleme,dis[0],dis[1]);
+            }
+        })
+    });
+
+    console.log(`done with toggleme, found ${items.length} ${plural("item",items.length)}`);
 }
 
 window.addEventListener('load', () => {
